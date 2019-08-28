77d0fbd50971932479e6d973587c78255e320207
che bin <ckqlss@gmail.com>
2019-08-28 19:05:07 +0900

    socket

--- client/feathers-client.js
+++ client/feathers-client.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 import { CookieStorage } from 'cookie-storage';
 
 const storage = new CookieStorage();
-const url = 'http://localhost/';
+const url = 'http://127.0.0.1/';
 const socket = io(url, { transports: ['websocket'] });
 const app = feathers()
   .configure(socketio(socket))
