2279e65eca3edc159a7ea813ba0b367d3fc9d19a
che bin <ckqlss@gmail.com>
2019-08-31 12:35:53 +0900

    3000 port

--- client/feathers-client.js
+++ client/feathers-client.js
@@ -5,8 +5,7 @@ import io from 'socket.io-client';
 import { CookieStorage } from 'cookie-storage';
 
 const storage = new CookieStorage();
-const url = process.env.NODE_ENV === 'production' ? 'http://127.0.0.1/' : process.env.apiURL;
-const socket = io(url, { transports: ['websocket'] });
+const socket = io(process.env.apiURL, { transports: ['websocket'] });
 const app = feathers()
   .configure(socketio(socket))
   .configure(auth({ storage }));
